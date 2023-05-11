b798c2afa5369e702faf54aa1d42665fb93b6b3e
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-05-11 23:08:30 +0800

    Web Usaha Mobil

---
+++ web-usaha-mobil/script.js
@@ -0,0 +1,22 @@
+// console.log('Hello World!');
+// console.log(window.scrollY);
+
+// document.addEventListener('scroll', () => {
+//     console.log(window.scrollY)
+// })
+
+const navClick = (value) => {
+    const component = document.getElementById(`content${value}`);
+    component.scrollIntoView({
+        behavior: 'smooth'
+    });
+
+    // console.log(component.id);
+    // if (component.id === 'content2') {
+    //     console.log('anu2');
+    //     window.scrollTo({
+    //         top: component.id,
+    //         behavior: "smooth"
+    //     })
+    // }
+}
