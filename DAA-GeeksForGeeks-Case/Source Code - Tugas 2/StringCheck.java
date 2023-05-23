8779eb3a100dded29e4827a17d313ac2a5095367
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-05-23 21:21:44 +0800

    Tambah Deskripsi

--- DAA-GeeksForGeeks-Case/Source Code - Tugas 2/StringCheck.java
+++ DAA-GeeksForGeeks-Case/Source Code - Tugas 2/StringCheck.java
@@ -2,7 +2,7 @@
 // Link: https://www.geeksforgeeks.org/check-given-string-sum-string/
 
 public class StringCheck {
-    // Fungsi untuk menjumlahkan dua bilangan dalam bentuk String
+    // Method/Fungsi untuk menjumlahkan dua bilangan dalam bentuk string
     public static String string_sum(String str1, String str2) {
         if (str1.length() < str2.length()) {
             String temp = str1;
@@ -13,9 +13,10 @@ public class StringCheck {
         int n = str2.length();
         String ans = "";
 
+        // Inisialisasi nilai awal dengan 0
         int carry = 0;
         for (int i = 0; i < n; i++) {
-            // Menjumlahkan digit saat ini dari kedua string
+            // Menjumlahkan digit/angka saat ini dari kedua string
             int ds = ((str1.charAt(m - 1 - i) - '0') + (str2.charAt(n - 1 - i) - '0') + carry) % 10;
             carry = ((str1.charAt(m - 1 - i) - '0') + (str2.charAt(n - 1 - i) - '0') + carry) / 10;
 
@@ -23,7 +24,7 @@ public class StringCheck {
         }
 
         for (int i = n; i < m; i++) {
-            // Menjumlahkan digit angka saat ini dengan carry jika ada
+            // Menjumlahkan digit saat ini dengan carry jika ada
             int ds = (str1.charAt(m - 1 - i) - '0' + carry) % 10;
             carry = (str1.charAt(m - 1 - i) - '0' + carry) / 10;
             ans = Character.toString((char)(ds + '0')) + ans;
@@ -36,7 +37,7 @@ public class StringCheck {
         return ans;
     }
 
-    // Fungsi untuk memeriksa apakah dua substring dapat menghasilkan jumlah yang benar
+    // Method/Fungsi lagi untuk memeriksa apakah dua substring dapat menghasilkan jumlah yang benar
     public static boolean checkSumStrUtil(String str, int beg, int len1, int len2) {
         String s1 = str.substring(beg, beg + len1);
         String s2 = str.substring(beg + len1, beg + len1 + len2);
@@ -44,12 +45,16 @@ public class StringCheck {
 
         int s3_len = s3.length();
 
-        if (s3_len > str.length() - len1 - len2 - beg)
+        // Cek dulu apakah jumlah digit s3 lebih besar dari sisa string yang tersedia
+        if (s3_len > str.length() - len1 - len2 - beg) {
+            // Berarti dia tidak dianggap benar
             return false;
+        }
 
+        // Memeriksa apakah s3 sama dengan substring berikutnya dalam string utama
         if (s3.equals(str.substring(beg + len1 + len2, beg + len1 + len2 + s3_len))) {
             if (beg + len1 + len2 + s3_len == str.length()) {
-                // Jika sudah mencapai akhir string, return true
+                // Jika sudah mencapai akhir string, maka langsung return true
                 return true;
             }
 
@@ -57,10 +62,10 @@ public class StringCheck {
             return checkSumStrUtil(str, beg + len1, len2, s3_len);
         }
 
-        return false;
+        return false; // Default return tetap dianggap false atau salah
     }
 
-    // Fungsi utama untuk memeriksa apakah string adalah sum-string atau bukan
+    // Method/Fungsi utama untuk memeriksa apakah string adalah sum-string atau bukan
     public static boolean isSumStr(String str) {
         int n = str.length();
 
@@ -72,17 +77,17 @@ public class StringCheck {
             }
         }
 
-        return false;
+        return false; // Default
     }
 
     public static void main(String[] args) {
         boolean result;
 
-        // Pengujian pertama
+        // Contoh pertama
         result = isSumStr("1212243660");
         System.out.println(result == true ? "True" : "False");
 
-        // Pengujian kedua
+        // Contoh pengujian kedua
         result = isSumStr("123456787");
         System.out.println(result == true ? "True" : "False");
     }
