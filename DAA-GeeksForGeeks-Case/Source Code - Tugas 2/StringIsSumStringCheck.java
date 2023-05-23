b1d6bebc4c348b3dbc0fc9f73fcc0f6f5ab8773c
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-05-23 21:34:34 +0800

    Lengkapi dokumentasi

--- DAA-GeeksForGeeks-Case/Source Code - Tugas 2/StringIsSumStringCheck.java
+++ DAA-GeeksForGeeks-Case/Source Code - Tugas 2/StringIsSumStringCheck.java
@@ -1,7 +1,9 @@
 // CASE: Check if a given string is sum-string
 // Link: https://www.geeksforgeeks.org/check-given-string-sum-string/
 
-public class StringCheck {
+// ALGORITMA BACKTRACKING -> Rekursif
+
+public class StringIsSumStringCheck {
     // Method/Fungsi untuk menjumlahkan dua bilangan dalam bentuk string
     public static String stringSum(String str1, String str2) {
         if (str1.length() < str2.length()) {
@@ -18,9 +20,11 @@ public class StringCheck {
         int carry = 0;
         for (int i = 0; i < n; i++) {
             // Menjumlahkan digit/angka saat ini dari kedua string
+            // Menggunakan .charAt() untuk mengakses tiap karakter di dalam String
             int ds = ((str1.charAt(m - 1 - i) - '0') + (str2.charAt(n - 1 - i) - '0') + carry) % 10;
             carry = ((str1.charAt(m - 1 - i) - '0') + (str2.charAt(n - 1 - i) - '0') + carry) / 10;
 
+            // Menggunakan .toString() untuk mengkonversi "int ds" sebelumnya menjadi String yang nanti akan di-return dalam method ini
             ans = Character.toString((char)(ds + '0')) + ans;
         }
 
@@ -55,14 +59,14 @@ public class StringCheck {
             return false;
         }
 
-        // Memeriksa apakah s3 sama dengan substring berikutnya dalam string utama
+        // Cek apakah s3 sama dengan substring berikutnya dalam string utama
         if (s3.equals(str.substring(beg + len1 + len2, beg + len1 + len2 + s3Len))) {
             if (beg + len1 + len2 + s3Len == str.length()) {
                 // Jika sudah mencapai akhir string, maka langsung return true
                 return true;
             }
 
-            // BRUT
+            // ===> BACKTRACKING -> Rekursif <===
             // Pemanggilan rekursif untuk memeriksa substring berikutnya
             return checkSumStrUtil(str, beg + len1, len2, s3Len);
         }
@@ -77,8 +81,9 @@ public class StringCheck {
         for (int i = 1; i < n; i++) {
             for (int j = 1; i + j < n; j++) {
                 // Memanggil fungsi checkSumStrUtil() untuk setiap kemungkinan substring
-                if (checkSumStrUtil(str, 0, i, j))
+                if (checkSumStrUtil(str, 0, i, j)) {
                     return true;
+                }
             }
         }
 
